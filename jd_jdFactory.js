216745dd60ec9a8e320c54c11811077fd1ca69b4
wishcaiji <3219904322@qq.com>
2021-02-25 22:18:18 +0800

    Update jd_jdFactory.js

--- jd_jdFactory.js
+++ jd_jdFactory.js
@@ -16,14 +16,14 @@
 ============Quantumultx===============
 [task_local]
 #京东工厂
-10 * * * * https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_jdfactory.js, tag=东东工厂, enabled=true
+10 * * * * https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/jd_jdfactory.js, tag=东东工厂, enabled=true
 ================Loon==============
 [Script]
-cron "10 * * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_jdfactory.js,tag=东东工厂
+cron "10 * * * *" script-path=https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/jd_jdfactory.js,tag=东东工厂
 ===============Surge=================
-东东工厂 = type=cron,cronexp="10 * * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_jdfactory.js
+东东工厂 = type=cron,cronexp="10 * * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/jd_jdfactory.js
 ============小火箭=========
-东东工厂 = type=cron,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_jdfactory.js, cronexpr="10 * * * *", timeout=200, enable=true
+东东工厂 = type=cron,script-path=https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/jd_jdfactory.js, cronexpr="10 * * * *", timeout=200, enable=true
  */
 const $ = new Env('东东工厂');
 
