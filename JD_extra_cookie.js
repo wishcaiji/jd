8a9dd214b6482f45a558b1de570581b8f46ad187
wishcaiji <3219904322@qq.com>
2021-02-25 11:54:55 +0800

    Update JD_extra_cookie.js

--- JD_extra_cookie.js
+++ JD_extra_cookie.js
@@ -17,13 +17,13 @@ hostname = wq.jd.com
 ===================Quantumult X=====================
 [rewrite_local]
 # 获取多账号京东Cookie
-https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion url script-request-header https://raw.githubusercontent.com/LXK9301/jd_scripts/master/JD_extra_cookie.js
+https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion url script-request-header https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/JD_extra_cookie.js
 ===================Loon===================
 [Script]
-http-request https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/JD_extra_cookie.js, tag=获取多账号京东Cookie
+http-request https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion script-path=https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/JD_extra_cookie.js, tag=获取多账号京东Cookie
 ===================Surge===================
 [Script]
-获取多账号京东Cookie = type=http-request,pattern=^https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/JD_extra_cookie.js,script-update-interval=0
+获取多账号京东Cookie = type=http-request,pattern=^https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wishcaiji/JD-scripts/master/JD_extra_cookie.js,script-update-interval=0
  */
 
 const APIKey = "CookiesJD";
